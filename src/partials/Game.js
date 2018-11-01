1259bdeefb7d877ec5ca38e1ab796b910c49ff34
Sophie Virtue <sophie.virtue@gmail.com>
2018-11-01 13:54:38 -0700

    add paddles to Paddle.js and Gae.js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,6 @@
-import { SVG_NS } from '../settings';
+import { SVG_NS, KEYS } from '../settings';
 import Board from './Board';
+import Paddle from './Paddle';
 
 export default class Game {
   constructor(element, width, height) {
@@ -7,10 +8,34 @@ export default class Game {
     this.width = width;
     this.height = height;
 
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.a,
+      KEYS.z
+	);
+	
+	this.player2 = new Paddle(
+		this.height,
+		this.paddleWidth,
+		this.paddleHeight,
+		(this.width - this.boardGap - this.paddleWidth),
+		((this.height - this.paddleHeight) / 2),
+		KEYS.up,
+		KEYS.down
+		);
+
     // Other code goes here...
     this.gameElement = document.getElementById(this.element);
     this.board = new Board(this.width, this.height);
-  }
+  } //end of constructor
 
   render() {
     // More code goes here...
@@ -20,6 +45,9 @@ export default class Game {
     svg.setAttributeNS(null, 'height', this.height);
     svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
-    this.board.render(svg);
+	this.board.render(svg);
+	this.player1.render(svg);
+	this.player2.render(svg);
+    // this.paddle.render(svg);
   }
 }
